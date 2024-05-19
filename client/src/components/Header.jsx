87c669692566ea0850204179fad767842cee7f6d
Sneha <janhvi9415@gmail.com>
2024-05-19 16:21:35 +0530

    complete signup page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 export default function Header() {
   return (
-    <header className="bg-slate-200" shadow-md>
+    <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to='/'>
           <h1 className="font-bold-text-sm:text-xl flex flex-wrap">
